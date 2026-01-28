adaaaaf4ac46044af3e9b16175a594af3983c7d6
Oscar Öjling <oscarojling@gmail.com>
2026-01-28 23:51:15 +0100

    Started making skeleton on node server

---
+++ scripts.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const pageTitle = (title) => {
+  return `<header><h1>${title}</h1></header>`;
+};
+
+const pageFooter = (footer) => {
+  return `<footer><h3>${footer}</h3></footer>`;
+};
+
+const navigation = () => {
+  return `<nav>
+    <a href="/">Home</a>
+    <a href="/artists">All Artists</a>
+    <a href="/pop">Pop Artists</a>
+    <a href="/hiphop">Hip-Hop Artists</a>
+  </nav>`;
+};
+
+http.createServer((req, res) => {
+  const address = url.parse(req.url, true);
+  const currentPath = address.pathname;
+  const queries = address.query;
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(navigation());
+
+  if (currentPath === "/") {
+    res.write(pageTitle("Home"));
+    res.write("<h2>Welcome to the site!</h2>");
+    res.write(pageFooter("Footer text"));
+    res.end();
+  }
+  if (currentPath === "/artists") {
+    res.write(pageTitle("All artists"));
+    res.write("<h2>Artist list here!</h2>");
+    res.write(pageFooter("Footer text"));
+    res.end();
+  }
+
+   if (currentPath === "/pop") {
+    res.write(pageTitle("Pop artists"));
+    res.write("<h2>Pop artists here!</h2>");
+    res.write(pageFooter("Footer text"));
+    res.end();
+  }
+
+  if (currentPath === "/hiphop") {
+    res.write(pageTitle("Hip-Hop artists"));
+    res.write("<h2>Hip-Hop artists here!</h2>");
+    res.write(pageFooter("Footer text"));
+    res.end();
+  }
+})
+
+.listen(3030, () => console.log("Listening on port 3030"));
+
