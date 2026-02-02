0447962c9a755080f458e00d7c981f12f084a0a2
Oscar Öjling <oscarojling@gmail.com>
2026-02-02 15:02:41 +0100

    Changed topic to spotify wrapped

--- scripts.js
+++ scripts.js
@@ -13,10 +13,10 @@ const pageFooter = (footer) => {
 const navigation = () => {
   return `<nav>
     <a href="/">Home</a>
-    <a href="/artists">All Artists</a>
-    <a href="/pop">Pop Artists</a>
-   <a href="/hiphop">Hip-Hop Artists</a>
-   <a href="/edm">EDM Artists</a>
+    <a href="/artists">Top Artists</a>
+    <a href="/songs">Top Songs</a>
+   <a href="/house">House Music</a>
+   <a href="/electronic">House Songs</a>
   </nav>`;
 };
 
@@ -29,37 +29,47 @@ http
     res.write(navigation());
 
     if (currentPath === "/") {
-      res.write(pageTitle("Home"));
+      res.write(pageTitle("My Spotify Wrapped 2025"));
       res.write("<h2>Welcome to the site!</h2>");
       res.write(pageFooter("Footer text"));
       res.end();
     }
     if (currentPath === "/artists") {
-      res.write(pageTitle("All artists"));
-      res.write("<h2>Artist list here!</h2>");
+      res.write(pageTitle("Top Artists"));
+      res.write("<h2>My top 5 artists of 2025!</h2>");
       res.write(pageFooter("Footer text"));
       res.end();
     }
 
-    if (currentPath === "/pop") {
-      res.write(pageTitle("Pop artists"));
-      res.write("<h2>Pop artists here!</h2>");
+    if (currentPath === "/songs") {
+      res.write(pageTitle("Top Songs"));
+      res.write("<h2>My top 10 songs of 2025!</h2>");
       res.write(pageFooter("Footer text"));
       res.end();
     }
 
-    if (currentPath === "/hiphop") {
-      res.write(pageTitle("Hip-Hop artists"));
-      res.write("<h2>Hip-Hop artists here!</h2>");
-      res.write(pageFooter("Footer text"));
-      res.end();
+    if (currentPath === "/house") {
+      fs.readFile("./content/page1.html", (err, data) => {
+        if (err) {
+          res.write("Could not load page!");
+        } else {
+          res.write(data);
+        }
+        res.write(pageFooter("Footer text"));
+        res.end();
+      });
     }
 
-    if (currentPath === "/edm") {
-      res.write(pageTitle("EDM artists"));
-      res.write("<h2>DJ:s here!</h2>");
-      res.write(pageFooter("Footer text"));
-      res.end();
+    if (currentPath === "/electronic") {
+      fs.readFile("./content/page2.html", (err, data) => {
+        if (err) {
+          res.write("Could not load page!");
+        } else {
+          res.write(data);
+        }
+        res.write(pageFooter("Footer text"));
+        res.end();
+      });
     }
   })
 
