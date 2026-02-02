4d6d64a2025df1713718178085af9e1d28d1687d
Oscar Öjling <oscarojling@gmail.com>
2026-02-02 15:53:15 +0100

    Added data files with arrays

---
+++ data/artists.js
@@ -0,0 +1,29 @@
+const topArtists = [
+  {
+    name: "KETTAMA",
+    genre: "house",
+    rank: 1
+  },
+  {
+    name: "SWIM",
+    genre: "house",
+    rank: 2
+  },
+   {
+    name: "Dj Billybool",
+    genre: "house",
+    rank: 3
+  },
+   {
+    name: "Men I Tust",
+    genre: "indie",
+    rank: 4
+  },
+   {
+    name: "Mall Grab",
+    genre: "house",
+    rank: 5
+  }
+];
+
+module.exports = topArtists
---
+++ data/songs.js
@@ -0,0 +1,64 @@
+const topSongs = [
+  {
+    songName: "Hell of a Ride",
+    artistName: "Nourished by Time",
+    genre: "indie",
+    rank: 1
+  },
+   {
+    songName: "Air Maxes - KETTAMA MIX",
+    artistName: "KETTAMA, Shady Nasty, Fred again",
+    genre: "house",
+    rank: 2
+  },
+   {
+    songName: "Faceless",
+    artistName: "Bubble Love, Ross from Friends",
+    genre: "house",
+    rank: 3
+  },
+   {
+    songName: "The Ride",
+    artistName: "SWIM",
+    genre: "house",
+    rank: 4
+  },
+   {
+    songName: "You Know What",
+    artistName: "N.E.R.D",
+    genre: "hip-hop",
+    rank: 5
+  },
+   {
+    songName: "DEMONER",
+    artistName: "Dj Billybool",
+    genre: "house",
+    rank: 6
+  },
+   {
+    songName: "Currency",
+    artistName: "ANOTR",
+    genre: "house",
+    rank: 7
+  },
+   {
+    songName: "BLOMSTERTID",
+    artistName: "Dj Billybool",
+    genre: "house",
+    rank: 8
+  },
+   {
+    songName: "Silent Prayer",
+    artistName: "Shanice",
+    genre: "r&b",
+    rank: 9
+  },
+   {
+    songName: "Hjerte Til Salg",
+    artistName: "Thor Farlov",
+    genre: "pop",
+    rank: 10
+  },
+]
+
+module.exports = topSongs
